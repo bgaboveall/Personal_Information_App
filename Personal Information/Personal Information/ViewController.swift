972dd44a4ad2fe7cfdd600e4c82558a6999ae24b
Boyko Avramov <boykoavramov@MacBook-Pro-2.local>
2017-01-16 17:05:21 -0500

    Initial comment

---
+++ Personal Information/Personal Information/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Personal Information
+//
+//  Created by Boyko Avramov on 2017-01-16.
+//  Copyright © 2017 Boyko Avramov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
